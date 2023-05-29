59e2b81e409ebf94fb3ca7af373c38031284bb1e
atomicules <base@atomicules.co.uk>
2023-05-29 19:13:14 +0100

    Copy highlights.scm from tree-sitter-java
    Also switch to [this tree-sitter][1] library.
    
    Having a play about...
    
    [1]: https://github.com/codieboomboom/tree-sitter-groovy/

--- Queries/highlights.scm
+++ Queries/highlights.scm
@@ -1,55 +1,141 @@
-; This is hitting all the package, class, def stuff:
-(unit
-  (identifier) @identifier.variable)
-(string
-  (identifier) @identifier.variable)
+; Methods
 
-(escape_sequence) @string.escape
+(method_declaration
+  name: (identifier) @function.method)
+(method_invocation
+  name: (identifier) @function.method)
+(super) @function.builtin
 
-(block
-  (unit
-    (identifier) @declaration
-  )
-)
+; Annotations
 
-(func
-  (identifier) @identifier.function
-)
+(annotation
+  name: (identifier) @attribute)
+(marker_annotation
+  name: (identifier) @attribute)
 
-(number) @value.number
+"@" @operator
 
-((identifier) @value.boolean
-  (#any-of? @value.boolean "true" "false" "True" "False"))
+; Types
 
-((identifier) @identifier.constant
-  (#lua-match? @identifier.constant "^[A-Z][A-Z%d_]*$"))
+(type_identifier) @type
 
-((identifier) @type.definition
-  (#any-of? @type.definition
-    "string"
-    "String"
-    "void"
-    "id"
-    "version"
-    "apply")
-)
+(interface_declaration
+  name: (identifier) @type)
+(class_declaration
+  name: (identifier) @type)
+(enum_declaration
+  name: (identifier) @type)
 
-((identifier) @keyword.construct
-  (#any-of? @keyword.construct
-    "static"
-    "class"
-    "def"
-    "import"
-    "new"
-    "package"
-))
+((field_access
+  object: (identifier) @type)
+ (#match? @type "^[A-Z]"))
+((scoped_identifier
+  scope: (identifier) @type)
+ (#match? @type "^[A-Z]"))
+((method_invocation
+  object: (identifier) @type)
+ (#match? @type "^[A-Z]"))
+((method_reference
+  . (identifier) @type)
+ (#match? @type "^[A-Z]"))
 
-(string) @string
+(constructor_declaration
+  name: (identifier) @type)
 
-(line_comment) @comment
-(block_comment) @comment
+[
+  (boolean_type)
+  (integral_type)
+  (floating_point_type)
+  (floating_point_type)
+  (void_type)
+] @type.builtin
 
-(operators) @operator
-(leading_key) @operator
+; Variables
 
-["(" ")" "[" "]" "{" "}"]  @bracket
+((identifier) @constant
+ (#match? @constant "^_*[A-Z][A-Z\\d_]+$"))
+
+(identifier) @variable
+
+(this) @variable.builtin
+
+; Literals
+
+[
+  (hex_integer_literal)
+  (decimal_integer_literal)
+  (octal_integer_literal)
+  (decimal_floating_point_literal)
+  (hex_floating_point_literal)
+] @number
+
+[
+  (character_literal)
+  (string_literal)
+] @string
+;(escape_sequence) @string.escape
+
+[
+  (true)
+  (false)
+  (null_literal)
+] @constant.builtin
+
+[
+  (line_comment)
+  (block_comment)
+] @comment
+
+; Keywords
+
+[
+  "abstract"
+  "assert"
+  "break"
+  "case"
+  "catch"
+  "class"
+  "continue"
+  "default"
+  "do"
+  "else"
+  "enum"
+  "exports"
+  "extends"
+  "final"
+  "finally"
+  "for"
+  "if"
+  "implements"
+  "import"
+  "instanceof"
+  "interface"
+  "module"
+  "native"
+  "new"
+  "non-sealed"
+  "open"
+  "opens"
+  "package"
+  "private"
+  "protected"
+  "provides"
+  "public"
+  "requires"
+  "return"
+  "sealed"
+  "static"
+  "strictfp"
+  "switch"
+  "synchronized"
+  "throw"
+  "throws"
+  "to"
+  "transient"
+  "transitive"
+  "try"
+  "uses"
+  "volatile"
+  "while"
+  "with"
+] @keyword
